9284d7d3781d2e80b39d2b2815069ef65259be79
Philipp Winter <phw@nymity.ch>
2023-12-07 08:51:16 +0100

    Update outdated URL.

--- popup.tsx
+++ popup.tsx
@@ -68,7 +68,7 @@ function ControlIcons() {
           <IconButton
             aria-label="Explore notes"
             size="small"
-            onClick={() => chrome.tabs.create({ url: "https://factcheck-web.nymity.ch" })}
+            onClick={() => chrome.tabs.create({ url: "https://factcheck.nymity.ch" })}
           >
             <ExploreIcon />
           </IconButton>
